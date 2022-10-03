844be9b89fef22d20109f1593692e7d9d745eeca
sutralia <sutralian@gmail.com>
2022-10-03 23:27:47 +0700

    fix: remove dist folder

--- dist/commodities/commodities-cli.d.ts
+++
@@ -1,7 +0,0 @@
-import { QuestionKeys } from '../interfaces/IQuestions';
-export declare const latestData: () => Promise<any>;
-export declare const getAreas: () => Promise<QuestionKeys[]>;
-export declare const getCommodityArea: (area: string) => Promise<any>;
-export declare const getCommodities: () => Promise<QuestionKeys[]>;
-export declare const getByCommodity: (commodity: string) => Promise<any>;
-export declare const getByRangePrice: (from: number, to: number) => Promise<any>;
--- dist/commodities/constant.d.ts
+++
@@ -1,2 +0,0 @@
-import { iQestions } from '../interfaces/IQuestions';
-export declare const questions: iQestions | any;
